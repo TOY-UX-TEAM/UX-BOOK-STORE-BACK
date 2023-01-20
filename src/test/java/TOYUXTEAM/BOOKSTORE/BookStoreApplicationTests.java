1a7a70d815abeefd0c8ea6cb3e0f14b1fdbb7f2f
gkfkktrh153 <gkfktkrh153@naver.com>
2023-01-20 15:49:19 +0900

    first commit

---
+++ src/test/java/TOYUXTEAM/BOOKSTORE/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package TOYUXTEAM.BOOKSTORE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
