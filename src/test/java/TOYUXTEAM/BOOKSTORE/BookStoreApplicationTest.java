f0f4291fdcf9af36ad14bd9aa9a566e7c200d11e
gkfkktrh153 <gkfktkrh153@naver.com>
2023-02-11 15:56:35 +0900

    initializer

---
+++ src/test/java/TOYUXTEAM/BOOKSTORE/BookStoreApplicationTest.java
@@ -0,0 +1,6 @@
+package TOYUXTEAM.BOOKSTORE;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BookStoreApplicationTest {
+  
+}
---
+++ src/test/java/TOYUXTEAM/BOOKSTORE/controller/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package TOYUXTEAM.BOOKSTORE.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@WebMvcTest
+class HelloControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    @DisplayName("/hello 요청시 hello world 출력")
+    void test() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello world"))
+                .andDo(MockMvcResultHandlers.print());
+
+    }
+}
