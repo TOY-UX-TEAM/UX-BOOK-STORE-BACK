3e8d93578256b64f17d89fe3b7ca3f05c251b5aa
gkfktkrh153 <77851079+gkfktkrh153@users.noreply.github.com>
2023-02-12 21:54:26 +0900

    Revert "Feature/4 book review api"

--- src/main/java/TOYUXTEAM/BOOKSTORE/domain/diary/service/DiaryService.java
+++
@@ -1,8 +0,0 @@
-package TOYUXTEAM.BOOKSTORE.domain.diary.service;
-
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class DiaryService {
-}
--- src/main/java/TOYUXTEAM/BOOKSTORE/domain/user/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package TOYUXTEAM.BOOKSTORE.domain.user.service;
-
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-}
