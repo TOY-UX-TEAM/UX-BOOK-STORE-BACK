dd152456906658adbe0e34f2f3386f493cf586c0
gkfkktrh153 <gkfktkrh153@naver.com>
2023-02-19 16:34:37 +0900

    BookReview -> BookReviewRes 변환 생성자

--- src/main/java/TOYUXTEAM/BOOKSTORE/domain/bookReview/dto/BookReviewRes.java
+++ src/main/java/TOYUXTEAM/BOOKSTORE/domain/bookReview/dto/BookReviewRes.java
@@ -1,5 +1,6 @@
 package TOYUXTEAM.BOOKSTORE.domain.bookReview.dto;
 
+import TOYUXTEAM.BOOKSTORE.domain.bookReview.model.BookReview;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,4 +15,13 @@ public class BookReviewRes {
     private String author;
     private String month;
     private String day;
+
+    public BookReviewRes(BookReview bookReview) {
+        title = bookReview.getTitle();
+        content = bookReview.getContent();
+        store = bookReview.getStore();
+        author = bookReview.getAuthor();
+        month = bookReview.getMonth();
+        day = bookReview.getDay();
+    }
 }
