425544b0d3738d6bb2ac59310c1371a6e145284d
gkfkktrh153 <gkfktkrh153@naver.com>
2023-02-19 17:48:39 +0900

    책 리뷰 사용자별 조회 레포지토리 쿼리 작성

--- src/main/java/TOYUXTEAM/BOOKSTORE/domain/bookReview/repository/BookReviewRepository.java
+++ src/main/java/TOYUXTEAM/BOOKSTORE/domain/bookReview/repository/BookReviewRepository.java
@@ -2,11 +2,19 @@ package TOYUXTEAM.BOOKSTORE.domain.bookReview.repository;
 
 
 import TOYUXTEAM.BOOKSTORE.domain.bookReview.model.BookReview;
+import TOYUXTEAM.BOOKSTORE.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookReviewRepository extends JpaRepository<BookReview, Long> {
 
+
+    @Query("select b from BookReview b where b.user.user_id = :userId")
+    List<BookReview> findAllByUser(@Param("userId") Long userId);
 
 }
