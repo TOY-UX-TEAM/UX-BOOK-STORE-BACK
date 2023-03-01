a655345369915a77533ac8cd4a914d8541081c29
gkfktkrh153 <gkfktkrh153@naver.com>
2023-03-01 21:10:08 +0900

    Form로그인 Controller작성

--- src/main/java/TOYUXTEAM/BOOKSTORE/domain/user/controller/UserController.java
+++ src/main/java/TOYUXTEAM/BOOKSTORE/domain/user/controller/UserController.java
@@ -5,13 +5,18 @@ import TOYUXTEAM.BOOKSTORE.domain.user.dto.RegisterUserReq;
 import TOYUXTEAM.BOOKSTORE.domain.user.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Collection;
 
 @Slf4j
@@ -26,6 +31,17 @@ public class UserController {
         Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
 
         return "user";
+    }
+    @GetMapping("/logout1")
+    public String logout(HttpServletRequest request, HttpServletResponse response)
+    {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null)
+        {
+            new SecurityContextLogoutHandler().logout(request,response,authentication);
+        }
+        System.out.println("logout");
+        return "logout";
     }
     @GetMapping("/manager")
     public String manager(){
