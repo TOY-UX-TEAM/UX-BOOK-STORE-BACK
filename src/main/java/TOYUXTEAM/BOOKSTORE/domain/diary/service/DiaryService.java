2400864bc6a9785a65e9b1ddf45be307d446de63
SoohoLee <hyeri0603@naver.com>
2023-03-09 17:08:32 +0900

    Date 로직 추가, 페이징 처리

--- src/main/java/TOYUXTEAM/BOOKSTORE/domain/diary/service/DiaryService.java
+++ src/main/java/TOYUXTEAM/BOOKSTORE/domain/diary/service/DiaryService.java
@@ -1,20 +1,22 @@
 package TOYUXTEAM.BOOKSTORE.domain.diary.service;
 
 import TOYUXTEAM.BOOKSTORE.domain.diary.dto.DiaryDto;
+import TOYUXTEAM.BOOKSTORE.domain.diary.dto.DiarySearchCond;
 import TOYUXTEAM.BOOKSTORE.domain.diary.model.Diary;
 import TOYUXTEAM.BOOKSTORE.domain.diary.repository.DiaryRepository;
 import TOYUXTEAM.BOOKSTORE.domain.user.model.User;
 import TOYUXTEAM.BOOKSTORE.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -25,25 +27,21 @@ public class DiaryService {
     private final DiaryRepository diaryRepository;
     private final UserRepository userRepository;
 
-    public List<DiaryDto> findDiaries(Long userId) {
-
-        User user = userRepository.findById(userId).get();
-        return user.getDiaries().stream()
-                .map(DiaryDto::new)
-                .collect(Collectors.toList());
+    public Page<DiaryDto> findDiaries(Long userId, int offset, int pageSize) {
+        Pageable pageable = PageRequest.of(offset, pageSize);
+        DiarySearchCond diarySearchCond = new DiarySearchCond(userId);
+        return diaryRepository.findByIdDiaries(diarySearchCond, pageable);
     }
 
-    public List<DiaryDto> findDiariesForDate(Long userId, Long month, Long day) {
+    public Page<DiaryDto> findDiariesForDate(Long userId, Long month, Long day, int offset) {
+        Pageable pageable = PageRequest.of(offset, 9);
         LocalDate date = LocalDate.of(LocalDateTime.now().getYear(), month.intValue(), day.intValue());
-        return diaryRepository.findByIdDiaryForDate(userId, date);
+        DiarySearchCond diarySearchCond = new DiarySearchCond(userId, date);
+        return diaryRepository.findByIdDiaryForDate(diarySearchCond, pageable);
     }
 
-    public DiaryDto findDiary(Long userId, Long diaryId) {
-
-        User user = userRepository.findById(userId).orElseThrow(() -> {
-            throw new EntityNotFoundException("존재하지 않는 회원입니다.");
-        });
-        return diaryRepository.findByDiary(diaryId);
+    public DiaryDto findDiary(Long diaryId) {
+        return diaryRepository.findByDiaryId(diaryId);
     }
 
     @Transactional
@@ -56,8 +54,7 @@ public class DiaryService {
         Diary diary = new Diary(diaryDto.getTitle(), diaryDto.getContent(), user);
         diaryRepository.save(diary);
 
-        diaryDto.dtoInSetDate(diaryRepository.findById(diary.getDiary_id()).get().getDiary_id(),
-                user, diary.getCreatedDate());
+        diaryDto.dtoInSetDate(diary.getId(), user.getUser_id(), diary.getCreatedDate());
 
         user.getDiaries().add(diary);
         return diaryDto;
@@ -74,9 +71,9 @@ public class DiaryService {
         });
         diary.diaryInSet(diaryDto.getTitle(), diaryDto.getContent());
         user.getDiaries().stream()
-                .filter(u -> u.getDiary_id().equals(diaryId))
+                .filter(u -> u.getId().equals(diaryId))
                 .forEach(u -> u.diaryInSet(diaryDto.getTitle(), diaryDto.getContent()));
-        diaryDto.dtoInSetDate(diaryId, user, diary.getCreatedDate());
+        diaryDto.dtoInSetDate(diaryId, user.getUser_id(), diary.getCreatedDate());
     }
 
     @Transactional
