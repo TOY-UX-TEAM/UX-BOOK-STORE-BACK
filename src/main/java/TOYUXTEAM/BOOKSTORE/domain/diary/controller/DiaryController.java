8f9a1ad7bdee9af8f225916da2cec3a866e8cf06
SoohoLee <hyeri0603@naver.com>
2023-03-12 20:54:15 +0900

    ResponseEntity 반환 객체 추가

--- src/main/java/TOYUXTEAM/BOOKSTORE/domain/diary/controller/DiaryController.java
+++ src/main/java/TOYUXTEAM/BOOKSTORE/domain/diary/controller/DiaryController.java
@@ -6,6 +6,7 @@ import TOYUXTEAM.BOOKSTORE.domain.diary.service.DiaryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,41 +19,41 @@ public class DiaryController {
     private final DiaryService diaryService;
 
     @GetMapping("/{id}/home")  // page 2
-    public Page<DiaryDto> getDiariesHome(@PathVariable("id") Long id, @RequestParam("page") int offset) {
-        return diaryService.findDiaries(id, offset, 2);
+    public ResponseEntity<Page<DiaryDto>> getDiariesHome(@PathVariable("id") Long id, @RequestParam("page") int offset) {
+        return ResponseEntity.ok(diaryService.findDiaries(id, offset, 2));
     }
 
     @GetMapping("/{id}/list")  // page 9
-    public Page<DiaryDto> getDiariesDateBefore(@PathVariable("id") Long id, @RequestParam("page") int offset) {
-        return diaryService.findDiaries(id, offset, 9);
+    public ResponseEntity<Page<DiaryDto>> getDiariesDateBefore(@PathVariable("id") Long id, @RequestParam("page") int offset) {
+        return ResponseEntity.ok(diaryService.findDiaries(id, offset, 9));
     }
 
 
     @GetMapping("/{id}/list/{month}/{day}") // page 9
-    public Page<DiaryDto> getDiariesForDate(@PathVariable("id") Long id,
+    public ResponseEntity<Page<DiaryDto>> getDiariesForDate(@PathVariable("id") Long id,
                                             @PathVariable("month") Long month, @PathVariable("day") Long day, @RequestParam("page") int offset) {
-        return diaryService.findDiariesForDate(id, month, day, offset);
+        return ResponseEntity.ok(diaryService.findDiariesForDate(id, month, day, offset));
     }
 
     @GetMapping("/diary")
-    public DiaryDto getDiary(@RequestParam("diaryId") Long diaryId) {
-        return diaryService.findDiary(diaryId);
+    public ResponseEntity<DiaryDto> getDiary(@RequestParam("diaryId") Long diaryId) {
+        return ResponseEntity.ok(diaryService.findDiary(diaryId));
     }
 
     @PostMapping("/{id}")
-    public DiaryDto saveDiary(@PathVariable("id") Long id, @RequestBody @Validated DiaryDto diaryDto) {
-        return diaryService.saveDiary(id, diaryDto);
+    public ResponseEntity<DiaryDto> saveDiary(@PathVariable("id") Long id, @RequestBody @Validated DiaryDto diaryDto) {
+        return ResponseEntity.ok(diaryService.saveDiary(id, diaryDto));
     }
 
     @PatchMapping("/{id}")
-    public DiaryDto modifyDiary(@RequestParam("diaryId") Long diaryId,
+    public ResponseEntity<DiaryDto> modifyDiary(@RequestParam("diaryId") Long diaryId,
                                 @PathVariable("id") Long id, @RequestBody @Validated DiaryDto diaryDto) {
         diaryService.updateDiary(id, diaryId, diaryDto);
-        return diaryDto;
+        return ResponseEntity.ok(diaryDto);
     }
 
     @DeleteMapping("/{id}")
-    public DiaryDto deleteDiary(@PathVariable("id") Long id, @RequestParam("diaryId") Long diaryId){
-        return diaryService.deleteDiary(id, diaryId);
+    public ResponseEntity<DiaryDto> deleteDiary(@PathVariable("id") Long id, @RequestParam("diaryId") Long diaryId){
+        return ResponseEntity.ok(diaryService.deleteDiary(id, diaryId));
     }
 }
