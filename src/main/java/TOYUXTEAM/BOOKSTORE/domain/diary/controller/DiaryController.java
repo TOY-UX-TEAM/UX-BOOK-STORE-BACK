2a5aa12474ee9454369665140df16bb80de5ad2a
SoohoLee <hyeri0603@naver.com>
2023-04-20 13:35:49 +0900

    feat: 로그인된 유저만 접근할 수 있도록 인증 어노테이션 추가 (#8)

--- src/main/java/TOYUXTEAM/BOOKSTORE/domain/diary/controller/DiaryController.java
+++ src/main/java/TOYUXTEAM/BOOKSTORE/domain/diary/controller/DiaryController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.IOException;
 
 @Slf4j
 @RestController
@@ -19,41 +22,53 @@ public class DiaryController {
 
     private final DiaryService diaryService;
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{id}/home")  // page 2
     public ResponseEntity<Page<DiaryResponse>> getDiariesHome(@PathVariable("id") Long id, @RequestParam("page") int offset) {
-        return ResponseEntity.ok().body(diaryService.findDiaries(id, offset, 2));
+        return ResponseEntity.ok().body(diaryService.findAll(id, offset, 2));
     }
-  
+
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{id}/list")  // page 9
     public ResponseEntity<Page<DiaryResponse>> getDiariesDateBefore(@PathVariable("id") Long id, @RequestParam("page") int offset) {
-        return ResponseEntity.ok().body(diaryService.findDiaries(id, offset, 9));
+        return ResponseEntity.ok().body(diaryService.findAll(id, offset, 9));
     }
 
-
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{id}/list/{month}/{day}/diary") // page 9
     public ResponseEntity<Page<DiaryResponse>> getDiariesForDate(@PathVariable("id") Long id,
                                             @PathVariable("month") Long month, @PathVariable("day") Long day, @RequestParam("page") int offset) {
-        return ResponseEntity.ok().body(diaryService.findDiariesForDate(id, month, day, offset));
+        return ResponseEntity.ok().body(diaryService.findByDate(id, month, day, offset));
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/diary")
     public ResponseEntity<DiaryResponse> getDiary(@RequestParam("diaryid") Long diaryId) {
-        return ResponseEntity.ok().body(diaryService.findDiary(diaryId));
+        return ResponseEntity.ok().body(diaryService.find(diaryId));
     }
 
-    @PostMapping("/{id}/newdiary")
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/{id}/new")
     public ResponseEntity<DiaryResponse> createDiary(@PathVariable("id") Long id, @RequestBody @Valid DiaryRequest diaryRequest) {
-        return ResponseEntity.ok().body(diaryService.createDiary(id, diaryRequest));
+        return ResponseEntity.ok().body(diaryService.create(id, diaryRequest));
     }
 
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/{id}/newfile")
+    public ResponseEntity<DiaryResponse> createDiaryWithFile(@PathVariable("id") Long id, @RequestBody @Valid DiaryRequest diaryRequest) throws IOException {
+        return ResponseEntity.ok().body(diaryService.createWithFile(id, diaryRequest));
+    }
+
+    @PreAuthorize("isAuthenticated()")
     @PatchMapping("/{id}/diary")
     public ResponseEntity<DiaryResponse> modifyDiary(@RequestParam("diaryid") Long diaryId,
                                 @PathVariable("id") Long id, @RequestBody @Valid DiaryRequest diaryRequest) {
-        return ResponseEntity.ok().body(diaryService.modifyDiary(id, diaryId, diaryRequest));
+        return ResponseEntity.ok().body(diaryService.modify(id, diaryId, diaryRequest));
     }
 
+    @PreAuthorize("isAuthenticated()")
     @DeleteMapping("/{id}/diary")
     public ResponseEntity<DiaryResponse> deleteDiary(@PathVariable("id") Long id, @RequestParam("diaryid") Long diaryId){
-        return ResponseEntity.ok().body(diaryService.deleteDiary(id, diaryId));
+        return ResponseEntity.ok().body(diaryService.delete(id, diaryId));
     }
 }
