dd92f88c311552d0f4ac172865ba8b7a8cd14ce7
SoohoLee <hyeri0603@naver.com>
2023-04-30 18:43:29 +0900

    feat: globalExceptionHandler 적용(#8)

--- src/main/java/TOYUXTEAM/BOOKSTORE/domain/diary/exception/DiaryException.java
+++ src/main/java/TOYUXTEAM/BOOKSTORE/domain/diary/exception/DiaryException.java
@@ -1,13 +1,14 @@
 package TOYUXTEAM.BOOKSTORE.domain.diary.exception;
 
+import TOYUXTEAM.BOOKSTORE.global.exception.ErrorCode;
 import TOYUXTEAM.BOOKSTORE.global.exception.baseException;
 import org.springframework.http.HttpStatus;
 
 public class DiaryException extends baseException {
 
-    private static final String code = "d0001";
+    private static final ErrorCode errorCode = ErrorCode.DIARY_NOT_FOUND;
 
     public DiaryException(String message) {
-        super(code, HttpStatus.BAD_REQUEST, message);
+        super(errorCode, HttpStatus.BAD_REQUEST, message);
     }
 }
