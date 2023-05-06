ec3290b095f5c1ed5936c9adffcd68cbdc70023e
SoohoLee <hyeri0603@naver.com>
2023-05-06 20:33:24 +0900

    feat: 에러코드 추가 (#8)

--- src/main/java/TOYUXTEAM/BOOKSTORE/global/exception/ErrorCode.java
+++ src/main/java/TOYUXTEAM/BOOKSTORE/global/exception/ErrorCode.java
@@ -6,8 +6,10 @@ import lombok.Getter;
 public enum ErrorCode {
 
     USER_NOT_FOUND(404, "ACCOUNT-001", "회원이 존재하지 않는 경우"),
-    MAXSIZE_OVER(400, "ACCOUNT-002", String.format("파일은 최대 %sMB 크기까지 입력할 수 있습니다.", 3)),
-    DIARY_NOT_FOUND(404, "DIARY-001", "일기가 존재하지 않는 경우");
+    USER_NOT_MATCH(403, "ACCOUNT-002", "회원 권한이 다를 경우"),
+    MAXSIZE_OVER(400, "ACCOUNT-003", String.format("파일은 최대 %sMB 크기까지 입력할 수 있습니다.", 3)),
+    DIARY_NOT_FOUND(404, "DIARY-001", "일기가 존재하지 않는 경우"),
+    DIARY_CONTENT_NOT_FOUND(404, "DIARY-002", "일기의 이미지가 존재하지 않는 경우");
 
     private final int status;
     private final String code;
